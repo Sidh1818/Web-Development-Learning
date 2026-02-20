9a72ba75b709b1f08be4cc6c7384a84f62a47383
Sidh Khurana <sidhkhurana06@gmail.com>
2026-02-20 13:46:01 +0530

    JS LAB

---
+++ JS/display.js
@@ -0,0 +1,17 @@
+/*const func()=>{
+let val1 = document.getElementById("i1").ariaValueMax;
+document.getElementById("i2").value=val1*12;
+    console.log(val1);
+}
+*/
+
+
+const f2i = () => {
+    let f = document.getElementById("i1").value;
+    document.getElementById("i2").value = f ? f * 12 : "";
+}
+
+const i2f = () => {
+    let i = document.getElementById("i2").value;
+    document.getElementById("i1").value = i ? (i / 12).toFixed(2) : "";
+}
